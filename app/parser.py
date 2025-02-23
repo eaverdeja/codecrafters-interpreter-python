3f2175bcdbde96b7b2b7fdaec62ce17cf72607c2
eaverdeja <eaverdeja@gmail.com>
2025-02-23 10:51:59 -0300

    Parser

---
+++ app/parser.py
@@ -0,0 +1,117 @@
+from dataclasses import dataclass
+
+from app.expr import Binary, Expr, Literal, Unary
+from app.scanner import Token, TokenType
+
+
+@dataclass
+class Parser:
+    """
+    Grammar
+
+    expression     → equality ;
+    equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+    comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+    term           → factor ( ( "-" | "+" ) factor )* ;
+    factor         → unary ( ( "/" | "*" ) unary )* ;
+    unary          → ( "!" | "-" ) unary | primary ;
+    primary        → NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" ;
+    """
+
+    tokens: list[Token]
+    _current: int = 0
+
+    def parse(self) -> Expr:
+        return self._expression()
+
+    def _expression(self) -> Expr:
+        return self._equality()
+
+    def _equality(self) -> Expr:
+        expr = self._comparison()
+
+        while self._match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL):
+            operator = self._previous()
+            right = self._comparison()
+            expr = Binary(expr, operator, right)
+
+        return expr
+
+    def _comparison(self) -> Expr:
+        expr = self._term()
+
+        while self._match(
+            TokenType.GREATER,
+            TokenType.GREATER_EQUAL,
+            TokenType.LESS,
+            TokenType.LESS_EQUAL,
+        ):
+            operator = self._previous()
+            right = self._term()
+            expr = Binary(expr, operator, right)
+
+        return expr
+
+    def _term(self) -> Expr:
+        expr = self._factor()
+
+        while self._match(TokenType.PLUS, TokenType.MINUS):
+            operator = self._previous()
+            right = self._factor()
+            expr = Binary(expr, operator, right)
+
+        return expr
+
+    def _factor(self) -> Expr:
+        expr = self._unary()
+
+        while self._match(TokenType.SLASH, TokenType.STAR):
+            operator = self._previous()
+            right = self._unary()
+            expr = Binary(expr, operator, right)
+
+        return expr
+
+    def _unary(self) -> Expr:
+        if self._match(TokenType.BANG, TokenType.MINUS):
+            operator = self._previous()
+            right = self._unary()
+            return Unary(operator, right)
+
+        return self._primary()
+
+    def _primary(self) -> Expr:
+        if self._match(TokenType.FALSE):
+            return Literal(False)
+        if self._match(TokenType.TRUE):
+            return Literal(True)
+        if self._match(TokenType.NIL):
+            return Literal(None)
+
+        raise ValueError("Unrecognized primary value")
+
+    def _match(self, *types: TokenType) -> bool:
+        for token_type in types:
+            if self._check(token_type):
+                self._advance()
+                return True
+        return False
+
+    def _check(self, token_type: TokenType) -> bool:
+        if self._is_at_end():
+            return False
+        return self._peek().token_type == token_type
+
+    def _advance(self) -> Token:
+        if not self._is_at_end():
+            self._current += 1
+        return self._previous()
+
+    def _is_at_end(self) -> bool:
+        return self._peek().token_type == TokenType.EOF
+
+    def _peek(self) -> Token:
+        return self.tokens[self._current]
+
+    def _previous(self) -> Token:
+        return self.tokens[self._current - 1]
