97760987e0b89076d824006b9253aca549620d40
eaverdeja <eaverdeja@gmail.com>
2025-02-23 10:52:08 -0300

    Entrypoint changes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import sys
 
-from app.scanner import Scanner
+from app.ast_printer import AstPrinter
+from app.expr import Expr
+from app.parser import Parser
+from app.scanner import Scanner, Token
 
 
 class Pylox:
@@ -11,18 +14,26 @@ class Pylox:
             self.run_prompt()
             exit(0)
 
-        if len(sys.argv) < 3:
-            print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
-            exit(1)
+        if len(sys.argv) == 2:
+            filename = sys.argv[1]
+            self.run_file(filename)
+            exit(0)
 
         command = sys.argv[1]
         filename = sys.argv[2]
 
-        if command != "tokenize":
-            print(f"Unknown command: {command}", file=sys.stderr)
-            exit(1)
-
-        self.run_file(filename)
+        match command:
+            case "tokenize":
+                tokens = self.scan_file(filename)
+                for token in tokens:
+                    print(token)
+            case "parse":
+                expr = self.parse_file(filename)
+                res = AstPrinter().print(expr)
+                print(res)
+            case _:
+                print(f"Unknown command: {command}", file=sys.stderr)
+                exit(1)
 
     def run_file(self, filename: str) -> None:
         with open(filename) as file:
@@ -48,11 +59,26 @@ class Pylox:
             self._had_error = False
 
     def run(self, source: str) -> None:
-        scanner = Scanner(source, error_reporter=self.error)
-        tokens = scanner.scan_tokens()
+        tokens = self._scan(source)
+        expr = self._parse(tokens)
+        res = AstPrinter().print(expr)
+        print(res)
 
-        for token in tokens:
-            print(token)
+    def scan_file(self, filename: str) -> list[Token]:
+        with open(filename) as file:
+            source = file.read()
+        return self._scan(source)
+
+    def parse_file(self, filename: str) -> Expr:
+        tokens = self.scan_file(filename)
+        return self._parse(tokens)
+
+    def _scan(self, source: str) -> list[Token]:
+        scanner = Scanner(source, error_reporter=self.error)
+        return scanner.scan_tokens()
+
+    def _parse(self, tokens: list[Token]) -> Expr:
+        return Parser(tokens).parse()
 
     def error(self, line: int, message: str) -> None:
         sys.stderr.write(f"[line {line}] Error: {message}\n")
