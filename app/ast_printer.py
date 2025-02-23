c74347939bee96eedb8fcd5f7508a94be112e5b2
eaverdeja <eaverdeja@gmail.com>
2025-02-23 09:49:22 -0300

    AstPrinter

---
+++ app/ast_printer.py
@@ -0,0 +1,38 @@
+from app.expr import Binary, Expr, Literal, Visitor
+from app.scanner import Token, TokenType
+
+
+class AstPrinter(Visitor[str]):
+    def print(self, expr: Expr) -> str:
+        return expr.accept(self)
+
+    def visit_binary_expr(self, expr: Binary) -> str:
+        return self._parenthisize(expr.operator.lexeme, expr.left, expr.right)
+
+    def visit_literal_expr(self, expr):
+        if expr.value is None:
+            return "nil"
+        return str(expr.value)
+
+    def _parenthisize(self, name: str, *exprs: Expr) -> str:
+        res = f"({name}"
+        for expr in exprs:
+            res += f" {expr.accept(self)}"
+        res += ")"
+
+        return res
+
+
+def main() -> None:
+    # Simple smoke test
+    expr: Expr = Binary(
+        Literal(2),
+        Token(TokenType.PLUS, "+", None, 1),
+        Literal(3),
+    )
+
+    print(AstPrinter().print(expr))
+
+
+if __name__ == "__main__":
+    main()
